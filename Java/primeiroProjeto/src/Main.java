e7b423b8409833aa463d7cc91f2558ff760d0eb8
Joaoaraujo61 <joaopedrodearaujo61@gmail.com>
2025-04-22 03:18:15 -0300

    casting in java

--- Java/primeiroProjeto/src/Main.java
+++ Java/primeiroProjeto/src/Main.java
@@ -19,5 +19,9 @@ public class Main {
                 Ano de lançamento:
                 """ + anoDeLancamento;
         System.out.println(sinopse);
+
+        int classificacao;
+        classificacao = (int) (media /2);
+        System.out.println(classificacao);
     }
 }
