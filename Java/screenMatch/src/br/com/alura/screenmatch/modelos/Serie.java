da79c7f6d03ec0cca743c24ddb855d5b50fca5e1
Joaoaraujo61 <joaopedrodearaujo61@gmail.com>
2025-05-02 02:32:03 -0300

    titulo added

---
+++ Java/screenMatch/src/br/com/alura/screenmatch/modelos/Serie.java
@@ -0,0 +1,40 @@
+package br.com.alura.screenmatch.modelos;
+
+public class Serie extends Titulo{
+    private int temporadas;
+    private boolean ativa;
+    private int episodiosPortemporaada;
+    private  int minPorEpisodio;
+
+    public int getTemporadas() {
+        return temporadas;
+    }
+
+    public boolean getAtiva(){
+        return ativa;
+    }
+
+    public int getEpisodiosPortemporaada(){
+        return episodiosPortemporaada;
+    }
+
+    public int getMinPorEpisodio(){
+        return minPorEpisodio;
+    }
+
+    public void setTemporadas(int temporadas){
+        this.temporadas = temporadas;
+    }
+
+    public  void setAtiva(boolean ativa){
+        this.ativa = ativa;
+    }
+
+    public void setEpisodiosPortemporaada(int episodiosPortemporaada){
+        this.episodiosPortemporaada = episodiosPortemporaada;
+    }
+
+    public void setMinPorEpisodio(int minPorEpisodio){
+        this.minPorEpisodio = minPorEpisodio;
+    }
+}
