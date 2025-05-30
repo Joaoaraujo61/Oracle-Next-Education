9cc2dcfa901de609f22d1f04ea5e9aee56c24c3e
Joaoaraujo61 <joaopedrodearaujo61@gmail.com>
2025-05-30 01:49:23 -0300

    .

--- Java/screenMatch/src/br/com/alura/screenmatch/principal/PrincipalComListas.java
+++ Java/screenMatch/src/br/com/alura/screenmatch/principal/PrincipalComListas.java
@@ -6,6 +6,8 @@ import br.com.alura.screenmatch.modelos.Titulo;
 
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
 
 public class PrincipalComListas {
     public static void main (String[] args){
@@ -17,7 +19,7 @@ public class PrincipalComListas {
         filmeJoao.avalia(10);
         Serie lost = new Serie("Lost",2000);
 
-        ArrayList<Titulo> lista = new ArrayList<>();
+        List<Titulo> lista = new ArrayList<>();
         lista.add(filmeJoao);
         lista.add(meuFilme);
         lista.add(outroFilme);
@@ -40,6 +42,10 @@ public class PrincipalComListas {
         System.out.println(buscaPorArtistas);
 
         Collections.sort(lista);
+        System.out.println(lista);
+
+        lista.sort(Comparator.comparing((Titulo::getAnoDeLancamento)));
+        System.out.println("Ordenando por ano: ");
         System.out.println(lista);
     }
 }
